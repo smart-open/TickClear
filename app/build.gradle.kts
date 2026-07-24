3aa9bb584e339e6a3cac4f6591c1d2dd5fa778b8
zhutw <tianwei7518@qq.com>
2026-07-24 15:46:33 +0800

    [config] versionCode 1->2 / versionName 1.0.0->2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.tickclear.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "2.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
