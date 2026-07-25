2b011b62b9118db26c973c16bc368364fd9fa412
zhutw <tianwei7518@qq.com>
2026-07-25 14:47:55 +0800

    [config] versionCode 8->9 / versionName 2.5.1->2.5.2 封板

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.tickclear.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 8
-        versionName = "2.5.1"
+        versionCode = 9
+        versionName = "2.5.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
