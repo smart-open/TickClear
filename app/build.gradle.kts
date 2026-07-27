bda41df2350ef90a794e8b10765431b49580e696
zhutw <tianwei7518@qq.com>
2026-07-27 22:38:49 +0800

    [config] 版本封板 v2.7.2（versionCode 15 / versionName 2.7.2）

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.tickclear.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 14
-        versionName = "2.7.1"
+        versionCode = 15
+        versionName = "2.7.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
